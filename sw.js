f847b4f7acb64006e452acc574a19b890aed3269
Neo Nie <neo.nie@autodesk.com>
2019-01-07 21:27:18 +0800

    Updates

--- sw.js
+++ sw.js
@@ -11,7 +11,8 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+importScripts("workbox-v3.6.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v3.6.1"});
 
 workbox.core.setCacheNameDetails({prefix: "gatsby-plugin-offline"});
 
@@ -25,30 +26,30 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8624ec4088fbf7dc9fc6.js"
+    "url": "webpack-runtime-5bf452824f513d3dbe10.js"
   },
   {
-    "url": "app-36a7258a391e5f3078f4.js"
+    "url": "app-6c14b02aba3c456d9fb2.js"
   },
   {
-    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-892c98c9df8e32c894ab.js"
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-502c29a2b54e9e1a636c.js"
   },
   {
     "url": "index.html",
-    "revision": "7016fce22b2b8374e998867a9823c61d"
+    "revision": "9633064ff10ea7a30d39f55e5916a2cd"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7068d7ae0d71e08907628aa08bbd467c"
+    "revision": "b428ee8427869e3255f433a636b0013b"
   },
   {
-    "url": "6-3d758e472b2fb20f9840.js"
+    "url": "6-34f7a89a418fcde18e8d.js"
   },
   {
-    "url": "component---src-pages-index-js-da443c0facdf53a31d0b.js"
+    "url": "component---src-pages-index-js-a763844a6e3331fbc74f.js"
   },
   {
-    "url": "0-22dab9a4194a2fff3e01.js"
+    "url": "0-ab64934947fa7abeb1b3.js"
   },
   {
     "url": "static/d/173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E.json",
@@ -75,3 +76,24 @@ workbox.routing.registerNavigationRoute("/stickyard/offline-plugin-app-shell-fal
 });
 
 workbox.routing.registerRoute(/\.(?:png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/, workbox.strategies.staleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/^https:/, workbox.strategies.networkFirst(), 'GET');
+"use strict";
+
+/* global workbox */
+self.addEventListener("message", function (event) {
+  var api = event.data.api;
+
+  if (api === "gatsby-runtime-cache") {
+    var resources = event.data.resources;
+    var cacheName = workbox.core.cacheNames.runtime;
+    event.waitUntil(caches.open(cacheName).then(function (cache) {
+      return Promise.all(resources.map(function (resource) {
+        return cache.add(resource).catch(function (e) {
+          // ignore TypeErrors - these are usually due to
+          // external resources which don't allow CORS
+          if (!(e instanceof TypeError)) throw e;
+        });
+      }));
+    }));
+  }
+});
